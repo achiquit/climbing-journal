11916bd8e12471aecbca11ab09607ac57e59d420
Andre Chiquito <85582759+achiquit@users.noreply.github.com>
2026-01-27 20:49:09 -0500

    Hotfix for to-csv script

--- scripts/send-to-csv.sql
+++ scripts/send-to-csv.sql
@@ -1,26 +1,26 @@
 .headers ON
 .mode csv
 
-.output my-climbing-journal/data/all-ticks.csv
+.output journal/data/all-ticks.csv
 .read scripts/all-ticks.sql
 
-.output my-climbing-journal/data/all-time-stats.csv
+.output journal/data/all-time-stats.csv
 .read scripts/stats-over-time.sql
 
-.output my-climbing-journal/data/partner-leaderboard.csv
+.output journal/data/partner-leaderboard.csv
 .read scripts/leaderboard.sql
 
-.output my-climbing-journal/data/climb-locs.csv
+.output journal/data/climb-locs.csv
 .read scripts/climbs-map.sql
 
-.output my-climbing-journal/data/monthly-height.csv
+.output journal/data/monthly-height.csv
 .read scripts/monthly-histogram.sql
 
-.output my-climbing-journal/data/yearly-height.csv
+.output journal/data/yearly-height.csv
 .read scripts/yearly-height.sql
 
-.output my-climbing-journal/data/top-climbs.csv
+.output journal/data/top-climbs.csv
 .read scripts/top-climbs.sql
 
-.output my-climbing-journal/data/ticks-by-grade.csv
+.output journal/data/ticks-by-grade.csv
 .read scripts/ticks-by-grade.sql
